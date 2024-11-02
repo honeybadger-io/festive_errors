ef692107358ce3292b48cc5a6b3b8c72daebf3d2
Joshua Wood <josh@joshuawood.net>
2024-11-02 10:12:19 -0700

    raise a test error in the dummy app

---
+++ test/dummy/app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < ApplicationController
+  def index
+    raise "happy holidays!"
+  end
+end
--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   mount FestiveErrors::Engine => "/festive_errors"
+  root to: "home#index"
 end
