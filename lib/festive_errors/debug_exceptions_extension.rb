f970feec8934e253f93d9a0f366d70ab9c38d53b
Joshua Wood <josh@joshuawood.net>
2024-11-02 11:03:31 -0700

    reload in dev

--- lib/festive_errors/debug_exceptions_extension.rb
+++ lib/festive_errors/debug_exceptions_extension.rb
@@ -5,12 +5,11 @@ module FestiveErrors
     def create_template(*)
       template = super
 
+      css_path = File.expand_path("../styles/christmas.css", __dir__)
+      css_content = File.read(css_path)
+
       template.content_for :style do
-        <<~CSS.html_safe
-          header h1::after {
-            content: " 🎅";
-          }
-        CSS
+        css_content.html_safe
       end
 
       template
